4da57273e3f0a40a2a2861e4ab1fb15bf963beae
duckquypuf <superduck081@gmail.com>
2025-11-26 15:35:03 +0300

    Add isLiquid property to BlockType class

--- include/blockTypes.h
+++ include/blockTypes.h
@@ -11,16 +11,18 @@ public:
     std::string name;
     bool isTransparent;
     bool isAir;
+    bool isLiquid;
 
     BlockType() : textures({}), isSolid(false), name("air"), isAir(true) {}
 
-    BlockType(std::vector<unsigned int> textures, bool isSolid, std::string name, bool isTransparent = false, bool isAir = false)
+    BlockType(std::vector<unsigned int> textures, bool isSolid, std::string name, bool isTransparent = false, bool isAir = false, bool isLiquid = false)
     {
         this->textures = textures;
         this->isSolid = isSolid;
         this->name = name;
         this->isTransparent = isTransparent;
         this->isAir = isAir;
+        this->isLiquid = isLiquid;
     }
 
     bool operator==(BlockType other)
